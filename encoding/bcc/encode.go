baa6113f15926da29c7722c43a2b51d914104ced
Shing Hoi Hung <leo@smartretail.co>
2019-09-18 10:52:18 +0800

    add bcc and bcd

---
+++ encoding/bcc/encode.go
@@ -0,0 +1,13 @@
+/*
+Block check character.
+In telecommunications, a block check character (BCC) is a character added to a transmission block to facilitate error detection.
+In longitudinal redundancy checking and cyclic redundancy checking, block check characters are computed for, and added to, each message block transmitted.
+*/
+package bcc
+
+func Encode(bytes []byte) (sum byte) {
+	for _, aByte := range bytes {
+		sum ^= aByte
+	}
+	return sum
+}
---
+++ encoding/bcd/encode.go
@@ -0,0 +1,31 @@
+/*
+Binary coded decimal (BCD) is a system of writing numerals that assigns a four-digit binary code to each digit 0 through 9 in a decimal (base-10) numeral.
+The four-bit BCD code for any particular single base-10 digit is its representation in binary notation, as follows: 0 = 0000. 1 = 0001. 2 = 0010.
+*/
+package bcd
+
+import (
+	"strconv"
+)
+
+func Encode(x int) (y int) {
+	s := strconv.Itoa(x)
+	for i, r := range s {
+		_r, _ := strconv.Atoi(string(r))
+		y = y + _r<<(4*(len(s)-1-i))
+	}
+	return y
+}
+
+func Decode(x int) (y int) {
+	var _s int
+	var _y string
+	s := strconv.Itoa(x)
+	for i, _ := range s {
+		_x := (x - _s) >> (4 * (len(s) - 1 - i))
+		_s = _s + _x<<(4*(len(s)-1-i))
+		_y = _y + strconv.Itoa(_x)
+	}
+	y, _ = strconv.Atoi(_y)
+	return y
+}
